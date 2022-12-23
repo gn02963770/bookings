5b55c9dadbce1695ec31c8f8a268d44d284634aa
gn02963770 <gn02963770@gmail.com>
2022-12-23 15:41:07 +0800

    authentication

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 
+	"github.com/gn02963770/bookings/helpers"
 	"github.com/justinas/nosurf"
 )
 
@@ -22,4 +23,14 @@ func NoSurf(next http.Handler) http.Handler {
 // SessionsLoad loads and saves the session on every request
 func SessionLoad(next http.Handler) http.Handler {
 	return session.LoadAndSave(next)
+}
+
+func Auth(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !helpers.IsAuthenticated(r) {
+			session.Put(r.Context(), "error", "Log in first!")
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
 }
